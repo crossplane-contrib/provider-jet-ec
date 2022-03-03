501f12d0d49c1ea1517de4c62a7657214bc524f0
Taylor Thornton <thornton.tn@gmail.com>
2022-03-03 15:09:05 -0800

    move strings to const
    Signed-off-by: Taylor Thornton <thornton.tn@gmail.com>

--- config/ec/deployment.go
+++ config/ec/deployment.go
@@ -6,30 +6,41 @@ import (
 	"github.com/crossplane/terrajet/pkg/config"
 )
 
+const (
+	deployment    = "ec_deployment"
+	elasticsearch = "elasticsearch"
+
+	esUsername    = "elasticsearch_username"
+	esPassword    = "elasticsearch_password"
+	httpEndpoint  = "http_endpoint"
+	httpsEndpoint = "https_endpoint"
+	endpointFmt   = "%s_%d"
+)
+
 // Configure configures individual resources by adding custom
 // ResourceConfigurators.
 func Configure(p *config.Provider) {
-	p.AddResourceConfigurator("ec_deployment", func(r *config.Resource) {
+	p.AddResourceConfigurator(deployment, func(r *config.Resource) {
 		r.ExternalName = config.IdentifierFromProvider
 
 		r.Sensitive = config.Sensitive{
 			AdditionalConnectionDetailsFn: func(attr map[string]interface{}) (map[string][]byte, error) {
 				conn := map[string][]byte{}
-				if a, ok := attr["elasticsearch_username"].(string); ok {
-					conn["elasticsearch_username"] = []byte(a)
+				if a, ok := attr[esUsername].(string); ok {
+					conn[esUsername] = []byte(a)
 				}
-				if a, ok := attr["elasticsearch_password"].(string); ok {
-					conn["elasticsearch_password"] = []byte(a)
+				if a, ok := attr[esPassword].(string); ok {
+					conn[esPassword] = []byte(a)
 				}
 
-				if a, ok := attr["elasticsearch"].([]interface{}); ok {
+				if a, ok := attr[elasticsearch].([]interface{}); ok {
 					for i, nestedAttr := range a {
 						if nestedMap, ok := nestedAttr.(map[string]interface{}); ok {
-							if httpEndpoint, ok := nestedMap["http_endpoint"].(string); ok {
-								conn[fmt.Sprintf("http_endpoint_%d", i)] = []byte(httpEndpoint)
+							if httpEndpoint, ok := nestedMap[httpEndpoint].(string); ok {
+								conn[fmt.Sprintf(endpointFmt, httpEndpoint, i)] = []byte(httpEndpoint)
 							}
-							if httpsEndpoint, ok := nestedMap["https_endpoint"].(string); ok {
-								conn[fmt.Sprintf("https_endpoint_%d", i)] = []byte(httpsEndpoint)
+							if httpsEndpoint, ok := nestedMap[httpsEndpoint].(string); ok {
+								conn[fmt.Sprintf(endpointFmt, httpsEndpoint, i)] = []byte(httpsEndpoint)
 							}
 						}
 					}
